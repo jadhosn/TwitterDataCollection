b6691eb2c1fb5bf83f8b3783980d6f834d6a8d4b
JadHosn <Jadhosn13@#>
2018-04-02 14:50:37 -0700

    Matching Entries Reporting script

--- ID_Reader.py
+++ ID_Reader.py
@@ -3,13 +3,14 @@ import json
 id_jad=[]
 id_colin=[]
 
-f = open('28_2018_jad_data.json', 'r')
+#f = open('28_2018_jad_data.json', 'r')
+f = open('26.json', 'r')
 for line in f:
     tweets= json.loads(line)
     if tweets.has_key('id'):
         id_jad.append(tweets['id'])
 
-f = open('28_2018_colin_data.json', 'r')
+f = open('02_04_2018.json', 'r')
 for line in f:
     if line != '\n':
         tweets = json.loads(line)
